0942f7df8c0a0abc24958c03c73aa172eb6fba8f
Shashank Kumar <shashank.kumar.apc13@itbhu.ac.in>
2016-01-22 23:41:23 +0530

    Modified the response format and made it presentable

--- book_search/googlecutsomsearch.py
+++ book_search/googlecutsomsearch.py
@@ -14,7 +14,7 @@ class Search:
 #research papers using google scholar. 
 #Query is what you want to google .
 
-  def __init__(self,query):
+  def __init__(self,query,max_limits=5):
 
     self.query = query
     self.service = build("customsearch","v1",
@@ -35,26 +35,44 @@ class Search:
           num = self.max_limits,
           ).execute()
 
-    pprint.pprint(res)
+    research_data = 'The Best rated books based on your search are : \n'
+    unicode_data = res['items']
+    for d in unicode_data:
 
+      book_data += d['link'].encode("ascii","ignore") + '\n'
+      
+      #print book_data
+      return book_data
+    
   def research_paper_search(self):
-
-    #service = build("customsearch","v1",
-    #         developerKey = "AIzaSyCGD8RWG9IAkKw57aHJM2NVTO2nl72CKus")
 
     paper_query = self.query
 
     res = self.service.cse().list(
           q= paper_query,
           cx= "016910633958041350436:dgzalhpaidu",
-          num = self.max_limits,
+          num = self.max_limits+1,
 
           ).execute()
+    
+    research_data = 'Most Cited Researh Papers based on your search are : \n'
+    unicode_data = res['items'][1:]
+    for d in unicode_data:
 
-
-    pprint.pprint(res)
+      try:
+        data += d['pagemap']['itemlist'][0]['itemlistelement'].encode("ascii","ignore").split(',')[0] + ":   "
+      except:
+        research_data += 'Title not Found here is the link  :   '
+      research_data += d['formattedUrl'].encode("ascii","ignore") + '\n'
+      
+    #print research_data
+    #print res['items'][1]['kind'].encode("ascii","ignore")
+    #pprint.pprint(res)
+    return research_data
+    
 
 
 if __name__ == '__main__':
   s = Search("MachineLearning")
-  s.research_paper_search()
+  result = s.research_paper_search()
+  print result
